e9f88b6fe96a2c39524c0b776a506070bee7e04e
pljarlegan <pl.jarlegan@gmail.com>
2019-12-31 18:21:46 +0100

    ADD: Alert & display helper

---
+++ test/unit/stats-alert.test.js
@@ -0,0 +1,21 @@
+const sa = require("../../lib/helpers/stats-alert");
+
+
+describe("stats-alerts", () => {
+  "use strict";
+
+  test("get alert : no alert", () => {
+    const alerts = [];
+    expect(sa.getAlerts(alerts)).isPrototypeOf(String);
+    expect(sa.getAlerts(alerts)).toEqual("");
+  });
+
+  test("get alert : some alerts", () => {
+    const alerts = [];
+    alerts.push("pif", "paf", "pouf");
+    expect(sa.getAlerts(alerts)).isPrototypeOf(String);
+    expect(sa.getAlerts(alerts).split("\n").length).toEqual(4);
+    alerts.push("pwet");
+    expect(sa.getAlerts(alerts).split("\n").length).toEqual(5);
+  });
+});
